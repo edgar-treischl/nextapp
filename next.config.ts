b27110724cc3adad8880ad23bafb13e158055f89
edgar.treischl <edgar.treischl@isb.bayern.de>
2025-12-06 19:55:49 +0100

    fixing workflow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Enable static HTML export
+  output: "export",
+  basePath: "/nextapp",
+
+  // You can keep other config options here
+  // Example: basePath if deploying to GitHub Pages subpath
+  // basePath: "/my-repo-name",
 };
 
 export default nextConfig;
