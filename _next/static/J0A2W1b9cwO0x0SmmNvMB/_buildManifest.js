a58b77c5a147642e6ccc0aeedc482b45a6e41234
edgar.treischl <edgar.treischl@isb.bayern.de>
2025-12-07 09:06:48 +0100

    Updates

--- _next/static/J0A2W1b9cwO0x0SmmNvMB/_buildManifest.js
+++ _next/static/J0A2W1b9cwO0x0SmmNvMB/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/nextapp//_next/:path+",
+        "destination": "/nextapp/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
