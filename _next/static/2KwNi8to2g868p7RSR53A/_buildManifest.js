19714c3f311f9a84f00c87fe413b2133330b3a61
edgar.treischl <edgar.treischl@isb.bayern.de>
2025-12-07 09:45:16 +0100

    Updates

--- _next/static/2KwNi8to2g868p7RSR53A/_buildManifest.js
+++ _next/static/2KwNi8to2g868p7RSR53A/_buildManifest.js
@@ -1,4 +1,13 @@
 self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/8e24317599bfe5c8.js"
+  ],
+  "/_error": [
+    "static/chunks/cf93c418efe9dcf7.js"
+  ],
+  "/about": [
+    "static/chunks/479d5f17bb778c25.js"
+  ],
   "__rewrites": {
     "afterFiles": [],
     "beforeFiles": [
@@ -10,7 +19,9 @@ self.__BUILD_MANIFEST = {
     "fallback": []
   },
   "sortedPages": [
+    "/",
     "/_app",
-    "/_error"
+    "/_error",
+    "/about"
   ]
 };self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/chunks/cf93c418efe9dcf7.js
@@ -0,0 +1,6 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/c078f7d06a9d85e0.js",
+  "static/chunks/b3dbe4f93ed77104.js",
+  "static/chunks/e1554bd2315b68e8.js",
+  "static/chunks/turbopack-428cdd4084fcfe44.js"
+])
